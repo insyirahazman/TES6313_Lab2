aac2291563ac9ce3ccd0634580bb2e4fdcf57df1
Insyirah Azman <insyirazman@gmail.com>
2025-11-11 07:59:27 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,27 @@
-import streamlit as st 
+import streamlit as st
+import clips
+import logging
 
-st.title("🎈 My new app")
-if st.button("Show Message"): st.info("This is your message box!")
+# setup working environment
+logging.basicConfig(level=15, format='%(message)s')
+
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+# input
+name = st.text_input("Enter your name")
+
+# knwoledge base
+env.build('(deftemplate result (slot name))')
+env.assert_string(f'(result (name "{name}"))')
+# inference
+env.run()
+
+# output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name'])
+
+        st.write(results[0], "better output")
